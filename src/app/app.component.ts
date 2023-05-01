1e768455cfce47a3f1f6ed7a9ee60a4235ca330c
carlos <valdescarlos17@gmail.com>
2023-05-01 11:31:28 -0400

    web3 example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Web3Service } from './services/web3.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  
+  constructor(
+    public web3Service: Web3Service
+  ) {}
+
+  ngOnInit() {
+    this.web3Service.connectToWeb3Account();
+  }
 }
